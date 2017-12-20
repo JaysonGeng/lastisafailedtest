2e0e7900f695d70ae88d530eacbe4cb5103b4f13
MichaelODs <1013619679@qq.com>
2017-12-20 14:19:42 +0800

    this is a test3

--- src/Test/test.java
+++ src/Test/test.java
@@ -3,6 +3,7 @@ package Test;
 public class test {
     public static void main(String[] args) {
         System.out.println("This is a test!!!");
-        System.out.println("This is a test!!!");
+        System.out.println("This is a test!!!xixixi");
+
     }
 }
